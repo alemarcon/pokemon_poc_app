2bd2cc95e8272fc045099c598cefa2b02ee6c8e4
Alessandro Marcon <a.marcon@vetrya.com>
2021-11-03 22:43:57 +0100

    first commit

---
+++ pokemonPoc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokemonPoc
+//
+//  Created by Alessandro Marcon on 03/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
